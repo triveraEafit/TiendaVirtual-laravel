f17bb130517955f79ceec970d4aedddbccf9267c
triveraEafit <tomasgrivera7@gmail.com>
2026-08-11 20:16:34 -0500

    Tutorial de Laravel rutas, controladores y vistas

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\View\View;
+class HomeController extends Controller
+{
+    public function index(): View
+    {
+        return view('home.index');
+    }
+}
